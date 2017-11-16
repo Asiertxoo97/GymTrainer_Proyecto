8a1af897529ac85cbed55af8cbc01cf551b596a4
aaldekoaben <aaldekoaben@192.168.0.13>
2017-11-16 20:22:36 +0100

    Añadidas descripciones faltan las fotos en los botones

--- src/Ventanas/VentanaDecision.java
+++ src/Ventanas/VentanaDecision.java
@@ -2,17 +2,21 @@ package Ventanas;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import java.awt.Color;
 
-public class VentanaDecision extends JFrame {
+public class VentanaDecision extends JFrame implements ActionListener {
 
 	ImageIcon Imagen;
 	private JPanel contentPane;
@@ -58,14 +62,34 @@ public class VentanaDecision extends JFrame {
 		contentPane.add(panelCentro, BorderLayout.CENTER);
 		panelCentro.setLayout(null);
 		
-		JLabel lblGimnasio = new JLabel("");
-		lblGimnasio.setBounds(6, 6, 307, 380);
-		panelCentro.add(lblGimnasio);
+		JButton btnGimnasio = new JButton(new ImageIcon());
+		btnGimnasio.setBounds(6, 6, 375, 185);
+		panelCentro.add(btnGimnasio);
 		
-		JLabel lblCharlas = new JLabel("");
-		lblCharlas.setBounds(325, 6, 307, 380);
-		panelCentro.add(lblCharlas);
+		
+		JButton btnCharlas = new JButton("");
+		btnCharlas.setBounds(259, 203, 375, 185);
+		panelCentro.add(btnCharlas);
+		
+		JTextPane txtpnGimnasio = new JTextPane();
+		txtpnGimnasio.setBackground(Color.LIGHT_GRAY);
+		txtpnGimnasio.setText("   Pulsa en el botón \"Gimnasio\" para poder acceder a nuestro gimnasio \"GymTrainerUD\" en el que podrás realizar muchos tipos de ejercicios, como ejercicios de pecho, abdominales, tren inferior... hasta poder realizar tus propias rutinas. Todo eso solo registrandote de una forma sencilla y así uniéndote a nosotros.");
+		txtpnGimnasio.setBounds(393, 6, 228, 185);
+		panelCentro.add(txtpnGimnasio);
+		
+		JTextPane txtpnCharlas = new JTextPane();
+		txtpnCharlas.setBackground(Color.LIGHT_GRAY);
+		txtpnCharlas.setText("   Pulsa en el botón \"Charlas\" para poder acceder a nuestra sección de charlas en las que tendrás la posibilidad de inscribirte a diferentes tipos de charlas como charlas sobre el ejercicio físico, sobre la nutrición, sobre deportistas de élite... una forma interesante de informarte sobre estos temas. ¡Inscríbete!");
+		txtpnCharlas.setBounds(16, 203, 228, 185);
+		panelCentro.add(txtpnCharlas);
+		
 		
 		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
